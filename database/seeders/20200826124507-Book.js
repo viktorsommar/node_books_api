babc9031b72fb1516f7bdff9a7915ac19e0b7df5
viktor_sommar <vsommarbaillot@gmail.com>
2020-08-28 15:48:45 +0200

    Refactoring code.

--- database/seeders/20200826124507-Book.js
+++ database/seeders/20200826124507-Book.js
@@ -2,6 +2,23 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("Books", null, {});
+    await queryInterface.bulkDelete("Authors", null, {});
+    await queryInterface.bulkInsert("Authors", [
+      {
+        firstName: "Thomas",
+        lastName: "Ochman",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+
+    const authors = await queryInterface.sequelize.query(
+      `SELECT id from "Authors";`
+    )
+
+    const authorsRows = authors[0]
+
     await queryInterface.bulkInsert(
       "Books",
       [
@@ -9,11 +26,13 @@ module.exports = {
           title: "Learn NodeJS with CraftAcademy",
           createdAt: new Date(),
           updatedAt: new Date(),
+          AuthorId: authorsRows[0].id,
         },
         {
           title: "Learn Sequelize with Adi",
           createdAt: new Date(),
           updatedAt: new Date(),
+          AuthorId: authorsRows[0].id,
         },
       ],
       {}
